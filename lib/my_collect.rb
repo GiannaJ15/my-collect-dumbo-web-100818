f1c31740ba026643c67dec6c16413a581483b2f6
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-29 23:33:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,11 @@
 def my_collect(collection)
 i =0 
-altered_collection =[]
+alteredcollection =[]
 while i< collection.length 
   altered_collection << yield (collection[i])
   i += 1 
 end 
-altered_collection
+alteredcollection
 end 
 
 
-  my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |student|
-   student.split(" ").first
- end 
