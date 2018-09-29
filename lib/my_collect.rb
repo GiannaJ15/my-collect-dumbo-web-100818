9ab1a445cfd8c7bf9f46939fd8ae86e036b2c50f
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-29 23:35:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(collection)
 i =0 
 while i< collection.length 
 @alteredcollection =[]
-  altered_collection << yield(collection[i])
+  @altered_collection << yield(collection[i])
   i += 1 
 end 
-alteredcollection
+@alteredcollection
 end 
 
 
