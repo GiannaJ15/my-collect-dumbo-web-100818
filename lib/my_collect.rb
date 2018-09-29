a0310119a99221861086f0f679e1362d13378791
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-29 23:39:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,13 @@
 def my_collect(collection)
 i =0 
-while i< collection.length 
 alteredcollection =[]
-altered_collection << yield(collection[i])
+while i< collection.length 
+alteredcollection << yield(collection[i])
 i += 1 
 end 
 alteredcollection
 end 
+
 
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
